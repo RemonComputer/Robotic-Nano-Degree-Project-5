28c4f6a94f6417710573d896f98d2e263bd76bc7
Remon Kamal <RemonComputer@gmail.com>
2022-12-22 18:23:14 +0200

    Fixing the display_markers script issue

--- my_robot/scripts/add_markers.sh
+++ my_robot/scripts/add_markers.sh
@@ -4,4 +4,4 @@ xterm -e "roslaunch my_robot world.launch" &
 sleep 5
 xterm -e "roslaunch my_robot amcl.launch" &
 sleep 5
-xterm -e "rosrun add_markers add_markers"
+xterm -e "rosrun add_markers add_markers_with_time"
